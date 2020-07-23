7b43324a319763b72d7381851cf48e8897e87b49
Your Name <64776125+nzmin714@users.noreply.github.com>
2020-07-23 13:58:47 +1200

    update website

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          MSA 2020 DevOps
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          MSA 2020!!!
         </a>
       </header>
     </div>
